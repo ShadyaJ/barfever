79dd93e90aee39ba6c09b67e95cacbc0c3a1fb36
Shadya Jaquier <shadya.jaquier27@gmail.com>
2021-03-11 20:38:15 +0100

    Added url for devise in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://git.heroku.com/barfever.git" }
+  config.action_mailer.default_url_options = { host: "https://barfever.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
