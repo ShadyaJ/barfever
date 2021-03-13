cb69f290da1c5aa8bf37bfa516711b1080993bef
Shadya Jaquier <shadya.jaquier27@gmail.com>
2021-03-13 13:36:27 +0100

    Added validations & appartenances to models

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,8 +1,7 @@
 class Bar < ApplicationRecord
   belongs_to :user
-  validates :name, :category, :description, :price, :music_style, :address_street, :adress_zipcode, :address_city, presence: true
+  has_many :bar_musics
+  validates :name, :category, :description, :price, :address_street, :address_zipcode, :address_city, presence: true
   validates :category, inclusion: { in:
     ['Bar', 'Pub', 'Nightclub', 'Loundge', 'Beach Bar', 'Champagne Bar', 'Cocktail Bar', 'Shot Bar', 'Beer Bar', 'Shisha Bar', 'Sports Bar', 'Gay Bar', 'Concert hall', 'Karaoke Bar', 'Rooftop'] }
-  validates :music_style, inclusion: { in:
-    ['French pop', '80s Music', '70s Music', 'Disco', 'Hip-Hop', 'Pop', 'Latino', 'Dance/Electro', 'Rock', 'Indie', 'Alternative', 'Classique', 'Jazz', 'Soul', 'RnB', 'Funk', 'Metal', 'Loundge Music'] }
 end
