359315fba885fd541c7850b5651339989cef45bf
reschahmadi <ahm.res.ahmadi@gmail.comi>
2021-03-13 22:36:36 +0100

    Added layout changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  layout 'application'
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :status, :phone_number])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bars, dependent: :destroy
-  validates :first_name, :last_name, :phone_number, :email, :nickname, :status, presence: true
-  validates :phone_number, :email, :nickname, uniqueness: true
+  validates :first_name, :last_name, :phone_number, :email, :status, presence: true
+  validates :phone_number, :email, uniqueness: true
   validates :status, inclusion: { in:
     ['I want to party!', 'I own the party!'] }
 end
