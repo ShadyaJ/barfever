846d3f972ece40444a29d4db7a06fb1831bdbaa2
Shadya Jaquier <shadya.jaquier27@gmail.com>
2021-03-13 12:03:18 +0100

    Created model Music

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_105851) do
+ActiveRecord::Schema.define(version: 2021_03_13_110151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2021_03_13_105851) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_bars_on_user_id"
+  end
+
+  create_table "musics", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
