484b9be2c064d931ed4da69109142708a431f77b
Shadya Jaquier <shadya.jaquier27@gmail.com>
2021-03-13 13:46:09 +0100

    Corrected error with one category name

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -3,5 +3,5 @@ class Bar < ApplicationRecord
   has_many :bar_musics
   validates :name, :category, :description, :price, :address_street, :address_zipcode, :address_city, presence: true
   validates :category, inclusion: { in:
-    ['Bar', 'Pub', 'Nightclub', 'Loundge', 'Beach Bar', 'Champagne Bar', 'Cocktail Bar', 'Shot Bar', 'Beer Bar', 'Shisha Bar', 'Sports Bar', 'Gay Bar', 'Concert hall', 'Karaoke Bar', 'Rooftop'] }
+    ['Bar', 'Pub', 'Nightclub', 'Lounge', 'Beach Bar', 'Champagne Bar', 'Cocktail Bar', 'Shot Bar', 'Beer Bar', 'Shisha Bar', 'Sports Bar', 'Gay Bar', 'Concert hall', 'Karaoke Bar', 'Rooftop'] }
 end
