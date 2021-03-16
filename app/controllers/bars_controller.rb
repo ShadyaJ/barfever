df1bd31464b2d9c0da3a8209f8f50f04072a480c
mickael borrajo <mick.borrajo.ael@gmail.com>
2021-03-16 22:52:50 +0100

    folder card created in shared to store the cards, css and htlm for the card-bar-small done

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,10 +1,13 @@
 class BarsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
-    if params[:category].present?
-      @bars = Bars.where(category: params[:category])
-    else
-      @bars = Bars.all
-    end
+    # if params[:category].present?
+    #   @bars = Bars.where(category: params[:category])
+    # else
+    #   @bars = Bars.all
+    # end
+    # @bars = Bars.all
   end
 
   private
