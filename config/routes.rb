28b5fb91d912f3d6b2b7782fe723ae85d0b94df0
mickael borrajo <mick.borrajo.ael@gmail.com>
2021-03-20 16:49:11 +0100

    cards add to index and index bars modified to show the category of events

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :reviews, only: %i(new create)
-  resources :events, only: [:show]
+  resources :events, only: [:index, :show]
   get '/welcome', to: 'pages#welcome'
 
-  resources :bars, only:[:index, :show]
+  resources :bars, only:[:index, :show, :incoming_event]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
