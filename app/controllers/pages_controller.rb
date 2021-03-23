636a4bb64d28da655bf9cd3f567a7b34e5021d92
mickael borrajo <mick.borrajo.ael@gmail.com>
2021-03-23 22:07:44 +0100

    folders cards add to pages for the homepages cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :welcome ]
 
   def home
+    @bars = Bar.all
+    @events = Event.all
   end
 
   def welcome
   end
+
 end
