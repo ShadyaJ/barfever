4e726dea4198a356f86308eca1d85bb3af42297b
Martin Jobin <mjobin123@yahoo.co.uk>
2021-03-27 17:10:57 +0100

    fixed seeds for music and bar_music

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,6 +1,4 @@
 class Music < ApplicationRecord
-  has_many :bar_musics
+  has_many :bar_musics, dependent: :destroy
   validates :name, presence: true
-  validates :name, inclusion: { in:
-    ['French pop', '80s Music', '70s Music', 'Disco', 'Hip-Hop', 'Pop', 'Latino', 'Dance/Electro', 'Rock', 'Indie', 'Alternative', 'Classique', 'Jazz', 'Soul', 'RnB', 'Funk', 'Metal', 'Loundge Music'] }
 end
