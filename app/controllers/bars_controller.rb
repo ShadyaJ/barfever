6ccc9b2928f2e240948bda265a8239828b80b86a
Shadya Jaquier <shadya.jaquier27@gmail.com>
2021-04-02 23:16:43 +0200

    Fixed various issues

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -5,9 +5,9 @@ class BarsController < ApplicationController
     if params[:query].present?
       @bars = Bar.where("address_city ILIKE?", "%#{params[:query]}%")
     elsif params[:tag]
-      @bars = Bar.where(bars: { category: params[:tag] })
+      @bars = Bar.where(category: params[:tag])
     elsif params[:tag_2]
-      @bars = Bar.where(bars: { terrasse: params[:tag_2] })
+      @bars = Bar.where(terrace: params[:tag_2])
     else
       @bars = Bar.all
     end
